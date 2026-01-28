d71ac73a613b63e5d580c1be9554664641279d7d
unknown <asmtabhi@gmail.com>
2026-01-28 10:48:07 +0530

    Added DataType.js file

---
+++ DataType.js
@@ -0,0 +1,29 @@
+let Name= "Abhi"//String
+let Age = 20; //Number - Int
+let Voter = true //Boolean datatype (true/false)
+let Roll ;//Undefined
+let regNo = null; // Null value 
+
+
+//-----------------------------------//
+
+//String declare using "" or '' 
+//Int / Number declare without "" 
+//Max capacity of Int Nimber 2 to the power 53 (2^53)
+//Null => Standalone value
+//Undefined => Variable Declared but not initialized
+//symbol => unique
+
+//object => User defined or class type data type
+
+
+//---------------------------------------------------//
+
+console.log(typeof Name);//Output => string
+console.log(typeof Age);//Output => Number / Int
+console.log(typeof null);//Output => object **Very important
+console.log(typeof undefined);//Output => undefined
+
+
+
+
